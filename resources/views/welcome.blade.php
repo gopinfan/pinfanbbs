f064e07f37293fc3acda48042b9ba854d7c75943
gopinfan <gopinfan@gmail.com>
2018-09-05 08:19:27 +0000

    修改welcome模板

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PinFanBBS
                 </div>
 
                 <div class="links">

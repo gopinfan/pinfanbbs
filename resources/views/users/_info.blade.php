751b40a5fb21faa094deaaee91fa148ce6a78e66
雨中歌者 <esinger@qq.com>
2018-09-29 15:49:47 +0800

    修改个人信息区块显示内容

--- resources/views/users/_info.blade.php
+++ resources/views/users/_info.blade.php
@@ -6,6 +6,8 @@
                 <img src="{{$user->gravatar(100)}}" alt="{{$user->name}}" class="user-gravatar rounded-circle">
             </a>
             <h4 class="user-name mt-3">{{$user->name}}</h4>
+            <h5 class="user-name mt-3">{{$user->email}}</h5>
+            <h6 class="user-name mt-3">{{$user->created_at}}</h6>
         </div>
     </div>
 </div>
